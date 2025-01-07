06529c557f1c5261cbaf457bcd24a5b7394ff29c
Christoph <c.wagner.germany@gmail.com>
2025-01-07 16:18:03 +0100

    setUp

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,10 +1,24 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.contrib.auth.models import User
+from forum_app.models import Question
 
 class LikeTests(APITestCase):
     
     def test_get_like(self):
         url = "http://127.0.0.1:8000/api/forum/likes/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        
+class QuestionTests(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.question = Question.objects.create(title='Text Question', content='Test Content', author=self.user, category='frontend')
+        
+    def test_detail_question(self):
+        url = reverse('question-detail', kwargs={'pk': self.question.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
