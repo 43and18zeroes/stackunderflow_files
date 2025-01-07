53943b707a78b59e3c3ba06938cfb5e4fd69e906
Christoph <c.wagner.germany@gmail.com>
2025-01-07 16:00:31 +0100

    static url

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -5,11 +5,6 @@ from rest_framework import status
 class LikeTests(APITestCase):
     
     def test_get_like(self):
-        url = reverse('like-list')
+        url = "http://127.0.0.1:8000/api/forum/likes/"
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_likes(self):
-        url = reverse('like-list')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
