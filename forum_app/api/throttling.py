a84192c05b1bfd4c5db180ae7653dc4c37bba3c8
Christoph <c.wagner.germany@gmail.com>
2025-01-11 16:57:00 +0100

    deepdive

--- forum_app/api/throttling.py
+++ forum_app/api/throttling.py
@@ -8,4 +8,10 @@ class QuestionThrottle(UserRateThrottle):
         if request.method == "GET":
             return True
         
+        new_scope = 'question-' + request.method.lower()
+        if new_scope in self.THROTTLE_RATES:
+            self.scope = new_scope
+            self.rate = self.get_rate()
+            self.num_requests, self.durcation = self.parse_rate(self.rate)
+            
         return super().allow_request(request, view)
